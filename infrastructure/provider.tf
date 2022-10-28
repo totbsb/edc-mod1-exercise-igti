895e16b5711d5ffbbe48fab7d723da8da77da607
totbsb <totbsb@gmail.com>
2022-10-28 17:33:57 -0300

    Backend terraform s3 e pasta infrastructure

---
+++ infrastructure/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
+# Centralizar o arquivo de controle de estado do terraform
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-igti-thiago"
+    key = "state/igti/edc/mod1/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
